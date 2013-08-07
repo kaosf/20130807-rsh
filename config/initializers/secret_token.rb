806f7b9743f0b7d57835aafd320f589f8988a83c
ka <ka.kaosf@gmail.com>
2013-08-07 20:47:16 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KaHerokuTest::Application.config.secret_key_base = 'd270426e88fd9970fa332bb9314e4765795c7ff339a6402d8b5d3bf987b76544b2b606dd1cba9b8158a8ad0d5ff7d86f8a3ea9448b2655b49de7442c338c5145'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KaHerokuTest::Application.config.session_store :cookie_store, key: '_ka-heroku-test_session'
